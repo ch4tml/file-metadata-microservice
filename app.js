078f1eaf50c0d288ddd3ec41fd43bba106822331
Tom <t.m.lemanski@googlemail.com>
2016-02-11 19:52:48 +0000

    Refactored

--- app.js
+++ app.js
@@ -9,34 +9,14 @@ var path = require("path");
 var http = require("http");
 var multer = require("multer");
 var upload = multer({
-  dest: "uploads/",
   limits: {
-    fileSize: 1000
+    fileSize: 10000000
   }
-});
+}).single("user_file");
 
-// Database setup
-/*var mongo = require("mongodb").MongoClient;
-var mongoose = require("mongoose");
-var configDB = process.env.MONGOLAB_URI || "mongodb://localhost:27017/";
-mongoose.createConnection(configDB, (err) => {
-  if(err){
-    console.log("Error connnecting to DB");
-    throw err;
-  }
-  else
-    console.log("Successfully connected to DB");
-});
-*/
 // View engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-// TODO
-/*
-ADD FILE FILTER
-ADD ERROR HANDLING
-*/
 
 // All environments
 app.set('port', process.env.PORT || 8080);
@@ -54,8 +34,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get("/", routes.index); // Home route
-app.post("/", upload.single("user_file"), routes.submit);
-//app.get("/form", routes.forms); // Home route
+app.post("/", upload, routes.submit);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,5 @@
 "use strict";
 
-// Database setup
-//var mongoose = require("mongoose");
-//mongoose.connect(process.env.MONGOLAB_URI || "mongodb://localhost:27017/");
-
 /* GET home page */
 exports.index = function(req, res){
     res.render('index', {
@@ -13,20 +9,9 @@ exports.index = function(req, res){
 };
 
 exports.submit = function(req, res){
-    console.log(req.file);
     res.end(JSON.stringify({
         filename: req.file.originalname,
         filesizeInBytes: req.file.size,
         uploadedAt: new Date()
     }));
 };
-
-/* For forms tutorial */
-
-/*
-exports.forms = function(req, res){
-    res.render("forms", {
-        title: "Form Experiment",
-        author: "Tom"
-    });
-};*/
